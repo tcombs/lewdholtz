26db0da7c10b9febe9c6322f371e255bde339091
The Tyler Combs <tcombs101@gmail.com>
2015-02-27 20:15:51 +0000

    lou-ify

--- public/local/js/client.js
+++ public/local/js/client.js
@@ -12,13 +12,14 @@ $(function() {
         }, function(data) {
             if (data.err) {
                 //There was an error
-                toastr["error"]("Something went wrong. Lets blame Twitter.", "Opps!")
+                toastr["error"]("Something went wrong. I'll blame Twitter.", "Opps!")
+                updateText();
             }
             else {
                 //Everything is fine
                 toastr["success"]("That was a sweet tweet", "Nice!")
                 $('#tweet-text').val('');
-                $('#char-count').text('140');
+                updateText();
 
             }
             $('#tweet-icon').toggleClass('hidden');
@@ -27,17 +28,38 @@ $(function() {
         });
 
     });
+    
+    $('#lou-ify').on('click',function(){
+        var text = $('#tweet-text').val();
+        text = text.replace(/s/g, 'th');
+        text = text.replace(/S/g, 'Th');
+        text = text.replace(/z/g, 'th');
+        text = text.replace(/Z/g, 'Th');
+        $('#tweet-text').val(text);
+        updateText();
+    });
 
 
     $('#tweet-text').bind('input propertychange', function() {
+        updateText();
+    });
+    
+    var updateText = function(){
         var text = $('#tweet-text').val();
         var n = text.length;
         var left = 140 - n;
         if (left < 0) {
-            text = text.substring(0, 140);
-            $('#tweet-text').val(text);
-            left = 0;
+            //set to red
+            $('#char-sent').addClass('danger-text');
+            //disabled == true
+            $('#tweet-submit').prop('disabled', true);
+        }
+        else{
+            //set to normal color
+            $('#char-sent').removeClass('danger-text');
+            //disabled == false
+            $('#tweet-submit').prop('disabled', false);
         }
         $('#char-count').text(left);
-    });
+    }
 });
